2bcca31b175dd2132554a6ce0dc7367c16501a2b
Nina Adora <nirinakaes@gmail.com>
2026-07-05 13:00:46 +0300

    writing the endpoints and routes for getting all items and getting specified items from my local api

--- codes/app.py
+++ codes/app.py
@@ -6,15 +6,21 @@ app = Flask(__name__)
 
 
 ############################ GET METHOD #################################
+#####GET ALL ITEMS ####
+
+app.route('/items',methods =['GET'])
+def get_items():
+    return jsonify(store.get_all()),200
 
 
-app.route('',methods =['GET'])
-def get_inventory():
-
-
-    if not name:
-       return ('Product not found',404)
-    pass
+###GET SPECIFIC ITEM#######
+app.route('/items/<int:item_ID>', methods=['GET'])
+def get_item(item_ID):
+    item = store.get_by_ID(item_ID)
+    if not item:
+        return jsonify({'error':'Item not found'}),404
+    return jsonify(item),200
+   
 
 
 
